4ecbf956b26388c5d55381869339b8f9967b9008
LuisDNA1196 <luisdna96@gmail.com>
2025-03-24 21:45:12 -0600

    navbar y home actualizados

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,20 @@
 import { Link } from "react-router";
 
 const Home = () => {
+  const getGreeting = () => {
+    const hour = new Date().getHours();
+    if (hour < 12) return "Buenos días";
+    if (hour < 19) return "Buenas tardes";
+    return "Buenas noches";
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <h1 className="text-3xl font-bold mb-6 text-blue-700">
+        {getGreeting()} Dr(a)
       </h1>
       <p className="text-gray-600 text-lg mb-4">
-        Selecciona una opción para continuar:
+        Seleccione una opción para continuar:
       </p>
       <div className="flex space-x-6">
         <Link to="/registrar">
@@ -21,7 +29,7 @@ const Home = () => {
         </Link>
         <Link to="/imagenes">
           <button className="bg-gray-600 text-white px-6 py-3 rounded-lg hover:bg-gray-700 transition">
-            Imágenes
+            Medicamento
           </button>
         </Link>
       </div>
